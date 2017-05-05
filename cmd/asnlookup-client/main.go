ffce44c7e798f6d20883e1ea06538d4a94932505
Justin Azoff <justin.azoff@gmail.com>
2017-05-04 22:28:25 -0400

    initial grpc frontend

---
+++ cmd/asnlookup-client/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"golang.org/x/net/context"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/JustinAzoff/asnlookup/pb"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+//type AsnlookupClient interface {
+//        Hello(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*HelloReply, error)
+//        Lookup(ctx context.Context, in *LookupRequest, opts ...grpc.CallOption) (*LookupReply, error)
+//        LookupMany(ctx context.Context, opts ...grpc.CallOption) (Asnlookup_LookupManyClient, error)
+//        LookupBatch(ctx context.Context, in *LookupRequestBatch, opts ...grpc.CallOption) (*LookupReplyBatch, error)
+//}
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewAsnlookupClient(conn)
+	client.Hello(context.Background(), &pb.Empty{})
+
+	stream, err := client.LookupMany(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	waitc := make(chan struct{})
+	go func() {
+		for {
+			rec, err := stream.Recv()
+			if err == io.EOF {
+				// read done.
+				close(waitc)
+				return
+			}
+			if err != nil {
+				log.Fatalf("Failed to receive a record: %v", err)
+			}
+			fmt.Printf("%s\t%s\t%d\t%s\t%s\n", rec.Prefix, rec.Address, rec.As, rec.Owner, rec.Cc)
+		}
+	}()
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		ip := scanner.Text()
+		req := &pb.LookupRequest{Address: ip}
+		err := stream.Send(req)
+		if err != nil {
+			log.Fatalf("Failed to send: %v", req)
+		}
+	}
+	stream.CloseSend()
+	<-waitc
+}
---
+++ cmd/asnlookup-server/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"golang.org/x/net/context"
+
+	"github.com/JustinAzoff/asnlookup"
+	pb "github.com/JustinAzoff/asnlookup/pb"
+)
+
+const (
+	port = ":50051"
+)
+
+//type AsnlookupServer interface {
+//        Hello(context.Context, *Empty) (*HelloReply, error)
+//        Lookup(context.Context, *LookupRequest) (*LookupReply, error)
+//        LookupMany(Asnlookup_LookupManyServer) error
+//        LookupBatch(context.Context, *LookupRequestBatch) (*LookupReplyBatch, error)
+//}
+
+type asnlookupServer struct {
+	backend *asnlookup.AsnBackend
+}
+
+func (s *asnlookupServer) Hello(ctx context.Context, empty *pb.Empty) (*pb.HelloReply, error) {
+	log.Printf("Hello called!")
+	return &pb.HelloReply{
+		Message: "Hello!",
+	}, nil
+}
+func (s *asnlookupServer) Lookup(ctx context.Context, req *pb.LookupRequest) (*pb.LookupReply, error) {
+	rec, err := s.backend.Lookup(req.Address)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.LookupReply{
+		Address: rec.IP,
+		Prefix:  rec.Prefix,
+		As:      int32(rec.AS),
+		Owner:   rec.Owner,
+		Cc:      rec.CC,
+	}, nil
+}
+func (s *asnlookupServer) LookupMany(stream pb.Asnlookup_LookupManyServer) error {
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		rec, err := s.backend.Lookup(req.Address)
+		if err != nil {
+			return err
+		}
+		rep := &pb.LookupReply{
+			Address: rec.IP,
+			Prefix:  rec.Prefix,
+			As:      int32(rec.AS),
+			Owner:   rec.Owner,
+			Cc:      rec.CC,
+		}
+		if err := stream.Send(rep); err != nil {
+			return err
+		}
+	}
+}
+func (s *asnlookupServer) LookupBatch(ctx context.Context, req *pb.LookupRequestBatch) (*pb.LookupReplyBatch, error) {
+	return nil, fmt.Errorf("not yet")
+}
+
+func main() {
+	b, err := asnlookup.NewAsnBackend("asn.db", "asnames.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Listening on %s", port)
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	server := asnlookupServer{backend: b}
+
+	// Creates a new gRPC server
+	s := grpc.NewServer()
+	pb.RegisterAsnlookupServer(s, &server)
+	s.Serve(lis)
+}
